50c855b50df6432145d89bd6e0854f69c04c4ebe
Michael Jenkins <michael.jenkins@disney.com>
2023-02-04 00:36:39 -0800

    change loop to use region

--- scripts/stop-instances.sh
+++ scripts/stop-instances.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # stop all running instances in all regions
-for i in us-{east,west}-{1,2};
+for region in us-{east,west}-{1,2};
 do
-    aws ec2 stop-instances --region=${i} --instance-ids $(aws ec2 describe-instances --region=${i} --query 'Reservations[].Instances[][].InstanceId' --filters Name=instance-state-name,Values=running --output=text) --output=json || echo "ok";
+    echo "${region}";
+    aws ec2 stop-instances --region=${region} --instance-ids $(aws ec2 describe-instances --region=${region} --query 'Reservations[].Instances[][].InstanceId' --filters Name=instance-state-name,Values=running --output=text) --output=json || echo "ok";
 done
