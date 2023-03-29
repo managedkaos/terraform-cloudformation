57e1e35930f7e30b84ab8a0d8b6944c24087ef77
Michael Jenkins <michael.jenkins@disney.com>
2023-03-29 00:46:19 -0700

    update ab params

--- scripts/apache-bench.sh
+++ scripts/apache-bench.sh
@@ -4,7 +4,7 @@
 SERVERS=$(cat ${1})
 
 # Define the parameters for the Apache Bench command
-AB_PARAMS="-n 10 -c 5 -t 120"
+AB_PARAMS="-n 5 -c 5 -t 120"
 
 # Initialize the iteration counter
 ITERATION=0
@@ -14,10 +14,18 @@ while :; do
   # Increment the iteration counter
   ITERATION=$((ITERATION+1))
 
+  # Use the modulo operator to check if ITERATION is even or odd
+  if [ $((ITERATION % 2)) -eq 0 ]
+  then
+    ROUTE="wordpress/"
+  else
+    ROUTE="wordpress/index.php/sample-page/"
+  fi
+
   # Loop through each server and fork a process to send traffic to it using Apache Bench
   for server in $SERVERS; do
     echo "$(date) Iteration $ITERATION: Sending traffic to $server in background..."
-    /usr/bin/ab $AB_PARAMS "http://$server/wordpress/" > /dev/null 2>&1 &
+    /usr/bin/ab "${AB_PARAMS}" "http://$server/${ROUTE}" > /dev/null 2>&1 &
   done
 
   # Wait for all background processes to finish
@@ -27,6 +35,6 @@ while :; do
   # Output a message indicating that the iteration has finished
   echo "$(date) Iteration $ITERATION finished."
 
-  # Sleep for 5 seconds before starting the next iteration
-  sleep 5
+  # Sleep before starting the next iteration
+  sleep 120
 done
