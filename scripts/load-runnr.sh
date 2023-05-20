e64fc5e67af8a27523d2cf3b2458e791266508ee
Michael Jenkins <michael.jenkins@disney.com>
2023-05-20 12:13:35 -0700

    Add .txt for spider log

--- scripts/load-runnr.sh
+++ scripts/load-runnr.sh
@@ -5,5 +5,5 @@ TAG=$(date +%F-%H-%M-%S)
 # Run apache-bench.sh and spider.sh in parallel
 # and wait for both to finish before continuing
 ./apache-bench.sh PublicDnsName-*.txt > ab-${TAG}.txt &
-./spider.sh PublicDnsName-*.txt > spider-${TAG} &
+./spider.sh PublicDnsName-*.txt > spider-${TAG}.txt &
 wait
