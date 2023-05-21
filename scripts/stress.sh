7f5cc829154378d8487af3f0ec5f84055bf6491e
Michael Jenkins <michael.jenkins@disney.com>
2023-05-20 22:50:06 -0700

    Add some stress

---
+++ scripts/stress.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Read the list of servers from all files passed in on the command line
+SERVERS=$(cat ${@})
+
+# Initialize the iteration counter
+ITERATION=0
+
+# Loop indefinitely
+while :; do
+  # Increment the iteration counter
+  ITERATION=$((ITERATION+1))
+
+  # Loop through each server and fork a process to start the stress command
+  for server in $SERVERS; do
+    echo "$(date) Stress Iteration $ITERATION: $server"
+    ssh "${server}" 'nohup bash stress --cpu 1 --io 1 --hdd 1 --timeout 60 > stress.txt 2>&1 &'
+  done
+
+  # Wait for all background processes to finish
+  echo "$(date) Waiting for Stress to finish iteration $ITERATION..."
+  wait
+  rm -rvf ./ec2-* &> /dev/null
+
+  # Output a message indicating that the iteration has finished
+  echo "$(date) Stress iteration $ITERATION finished."
+
+  # Sleep before starting the next iteration
+  sleep 60
+done
