2670295293aee94d07304ed56538c25734b32368
Michael Jenkins <michael.jenkins@disney.com>
2023-05-22 10:24:28 -0700

    lower the load-runnr

--- scripts/load-runnr.sh
+++ scripts/load-runnr.sh
@@ -4,7 +4,7 @@ TAG=$(date +%F-%H-%M-%S)
 
 # Run apache-bench.sh and spider.sh in parallel
 # and wait for both to finish before continuing
-for i in {1..10}; do
+for i in {1..3}; do
   ./apache-bench.sh PublicDnsName-*.txt > ab${i}-${TAG}.txt &
   ./spider.sh PublicDnsName-*.txt > spider${i}-${TAG}.txt &
 done
